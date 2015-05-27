37ac9e1a4a6508013632320f36bc73be49f4d9cb
murajun1978 <crazy.rendezvous.m.jun@gmail.com>
2015-05-28 01:10:43 +0900

    default setting

--- config/application.rb
+++ config/application.rb
@@ -26,10 +26,24 @@ module PManager
     # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :ja
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.template_engine :haml
+      g.test_framework :rspec,
+        fixtures:         true,
+        view_specs:       false,
+        routing_specs:    false,
+        request_specs:    false,
+        helper_specs:     false,
+        controller_specs: true
+      g.fixture_replacement :factory_girl, dir: "spec/factories"
+    end
   end
 end
