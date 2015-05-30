891308c5247dcd12f0cef4d01424c9219c352e72
murajun1978 <crazy.rendezvous.m.jun@gmail.com>
2015-05-30 20:53:22 +0900

    remove unnecessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
   root to: redirect('/projects')
-  resources :projects, only: [:index, :show, :new, :edit, :destroy]
-  resources :projects, as: :project_forms, only: [:create, :update]
+  resources :projects
 end
